526525cbd8c82d550253fb7887091baccb3084fc
muggullas <60910214+muggullas@users.noreply.github.com>
2023-03-15 16:22:45 +0530

    Delete network.tf

--- network.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "devtest_vnet" {
-  name                = "mydevtestlab-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.devtest_rg.location
-  resource_group_name = azurerm_resource_group.devtest_rg.name
-}
