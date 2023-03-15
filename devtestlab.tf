0e562f8df67843f1758b744e8c95fc40de2ad1a8
muggullas <60910214+muggullas@users.noreply.github.com>
2023-03-15 16:22:32 +0530

    Delete devtestlab.tf

--- devtestlab.tf
+++
@@ -1,6 +0,0 @@
-
-resource "azurerm_dev_test_lab" "devtest_lab" {
-  name                = "mydevtestlab"
-  location            = azurerm_devtest_lab.devtest_lab.location
-  resource_group_name = azurerm_resource_group.devtest_rg.name
-}
